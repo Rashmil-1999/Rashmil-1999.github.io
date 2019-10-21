ee3b3cba30ba8075d841ea13d31abbf69e326c94
Rashmil Panchani <rashmilpanchani@Rashmils-MacBook-Pro.local>
2019-10-21 19:59:55 +0530

    Updates

---
+++ precache-manifest.4fcfaad8e3208899d2b47f53753b5462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86a127a62e2b4bf4e943790d9e94a68",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "b7f9ace3ba1d8629e53b",
+    "url": "/profile/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5e857dc789c5ec47adec",
+    "url": "/profile/static/js/2.95b9f86d.chunk.js"
+  },
+  {
+    "revision": "b7f9ace3ba1d8629e53b",
+    "url": "/profile/static/js/main.df75a47c.chunk.js"
+  },
+  {
+    "revision": "314df111e2131c7a4513",
+    "url": "/profile/static/js/runtime~main.a46398cd.js"
+  }
+]);
