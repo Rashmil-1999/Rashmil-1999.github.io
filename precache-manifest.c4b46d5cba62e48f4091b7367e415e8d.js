4b734588d914da59da765a3d0caea082a727af17
Rashmil Panchani <damonsalvatore833@gmail.com>
2020-10-12 12:51:11 +0530

    Updates

---
+++ precache-manifest.c4b46d5cba62e48f4091b7367e415e8d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6db86ab87ed00a4ccac53443c2e1c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "029a8cee8f54b49e7fc3",
+    "url": "/static/css/main.be93b5a4.chunk.css"
+  },
+  {
+    "revision": "0c0df08b424e03f246bc",
+    "url": "/static/js/2.8ef3bd3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8ef3bd3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "029a8cee8f54b49e7fc3",
+    "url": "/static/js/main.ef6006c3.chunk.js"
+  },
+  {
+    "revision": "e2c25ed786060b511c48",
+    "url": "/static/js/runtime-main.d6274831.js"
+  },
+  {
+    "revision": "1a3dcfdeb5cb53fa42085a1addf0e17d",
+    "url": "/static/media/aowe.1a3dcfde.jpg"
+  },
+  {
+    "revision": "d5222ae770f1ed8159f6cb748cac7905",
+    "url": "/static/media/archive.d5222ae7.jpeg"
+  },
+  {
+    "revision": "8db9168af95291853075d851d3333fa2",
+    "url": "/static/media/attendance.8db9168a.webp"
+  },
+  {
+    "revision": "fde8d792c21f1c6b5001384884d526bc",
+    "url": "/static/media/emotion_recognition.fde8d792.png"
+  },
+  {
+    "revision": "abd60565f3c97b27c708f0bb64f0f738",
+    "url": "/static/media/event.abd60565.png"
+  },
+  {
+    "revision": "3e10defac4f818ca6490d8fbad1a7cfb",
+    "url": "/static/media/profilepic.3e10defa.jpg"
+  },
+  {
+    "revision": "12bd9638b8e6af8bbb5f5e27b1a74c79",
+    "url": "/static/media/resume.12bd9638.png"
+  },
+  {
+    "revision": "66b6a13d014477ddb30cc4ba3f98daee",
+    "url": "/static/media/smgarden.66b6a13d.png"
+  }
+]);
